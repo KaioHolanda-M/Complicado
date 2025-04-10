a42ce6cea07694eb61b1908039485bc30021d263
KaioHolanda-M <viniciao79880@gmail.com>
2025-04-10 09:43:19 -0300

    Update script.js

--- nova-pasta/script.js
+++ nova-pasta/script.js
@@ -15,4 +15,24 @@ document.addEventListener('DOMContentLoaded', () => {
   chatContainer.scrollTop = chatContainer.scrollHeight;
 }
   sendButton.addEventListener('click', () => {
-  
+  const messageText = messageInput.value;
+  if (messageText.trim() !== '') {
+    addMessage( messageText, true);
+    
+  simulateAgentResponse(messageText);
+    
+   messageInput.value = '';
+  }
+});
+function simulateAgenteResponse(user message) {
+setTimeout(() => {
+  const responses = [
+    "Olá, como posso te ajudar hoje?"
+    ];
+  const randomIndex = Match.floor(Math.random() * responses.length);
+  const agentReponse = responses[randomIndex];
+  addMessage(agentResponse, false);
+}, 1000);
+}
+addMessage("Olá! Bem vindo! Como posso ajudar?", false);
+});
